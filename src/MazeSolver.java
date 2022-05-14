d991f21f3e3b9e0fc1dde0b506b4da590e65e8a3
abderrahman504 <ahmad1khayri2@gmail.com>
2022-05-14 21:49:09 +0200

    Added the file_to_strings() method. The method takes the maze file and returns the maze in the form of an array of strings.

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,12 +1,81 @@
 import DS.*;
+import java.awt.Point;
+import java.io.File;
+import java.util.Scanner;
 
 
 public class MazeSolver 
 {
-    public static void main(String[] args)
+    class Cell extends Point
     {
-        System.out.println("Hello, World!");
+        private Cell entry;
+        private boolean checked = false;
+
+        Cell(Cell entry, Point coordinates)
+        {
+            this.setLocation(coordinates);
+            this.entry = entry;
+        }
+
+        /**
+         * A cell is considered checked if the search algorithm has already visited the cell
+         * and added the cells that branch from it to a queue, stack or p. queue.
+         * @return true if the cell has been checked already and false otherwise.
+         */
+        boolean checked()
+        {
+            return checked;
+        }
+
+        /**
+         * 
+         * @return the cell that we used to enter this cell. In other words it returns the parent cell.
+         */
+        Cell entry()
+        {
+            return entry;
+        }
     }
+    /**
+     * Reads a maze from the file fMaze and returns it in the form of an array of strings. 
+     * Each string in the array is a line in the file. If something is wrong with the file then an exception is thrown.
+     * @param fMaze the file containing the maze.
+     * @return the contents of the file in the form of an array of strings.
+     * @throws Exception
+     */
+    private static String[] file_to_strings(File fMaze) throws Exception
+    {
+        String[] maze; 
+        Scanner scn = new Scanner(fMaze);
+        int height = scn.nextInt(), width = scn.nextInt();
+        maze = new String[height];
+        if (!scn.hasNextLine())
+        {
+            scn.close();
+            throw new Exception();
+        }
+        scn.nextLine();
+        
+        for (int i=0; i<height; i++)
+        {
+            maze[i] = scn.nextLine();
+            if (maze[i].length() != width)
+            {
+                scn.close();
+                throw new Exception();
+            }
+        }
+        if (scn.hasNextLine())
+        {
+            scn.close();
+            throw new Exception();
+        }
+
+
+        scn.close();
+        return maze;
+    }
+
 }
 
 
@@ -19,7 +88,7 @@ interface IMazeSolver
     *
     to point ’E’ inclusive, or null if no path is found.
     */
-    public int[][] solveBFS(java.io.File maze);
+    public int[][] solveBFS(File maze);
     /**
     * Read the maze file, and solve it using Depth First Search
     * @param maze maze file
@@ -27,10 +96,10 @@ interface IMazeSolver
     *
     to point ’E’ inclusive, or null if no path is found.
     */
-    public int[][] solveDFS(java.io.File maze);
+    public int[][] solveDFS(File maze);
 
 
-    public int[][] solveBestFS(java.io.File maze);
+    public int[][] solveBestFS(File maze);
 
 
 }
