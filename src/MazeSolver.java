7f0755eaad63c9b0284037e4240874019da33958
abderrahman504 <ahmad1khayri2@gmail.com>
2022-05-15 16:02:51 +0200

    Finished the bestFS search algorithm. <NEEDS TESTING> I finished the bestFS algorithm. I also made some tiny changes to the Cell class to make it static.

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -2,11 +2,12 @@ import DS.*;
 import java.awt.Point;
 import java.io.File;
 import java.util.Scanner;
+import java.lang.Math;
 
 
 public class MazeSolver //implememts IMazeSolver
 {
-    class Cell extends Point
+    static class Cell extends Point
     {
         private Cell entry;
 
@@ -25,6 +26,12 @@ public class MazeSolver //implememts IMazeSolver
             return entry;
         }
     }
+    enum Direction
+    {
+        left, right, up, down
+    }
+
+
     /**
      * Reads a maze from the file fMaze and returns it in the form of an array of strings. 
      * Each string in the array is a line in the file. If something is wrong with the file then an exception is thrown.
@@ -77,11 +84,35 @@ public class MazeSolver //implememts IMazeSolver
         catch (Exception e)
         {
             //Handle invalid file
-            return new int[1][1];
+            return new int[0][0];
         }
+        //---------------------------------------------------------------------------------------
+        Cell currentCell = find_path_bestFS(sMaze);
+        
+        Stack stck = new Stack();
+        while (currentCell.entry != null)
+        {
+            stck.push(currentCell);
+            currentCell = currentCell.entry();
+        }
+        stck.push(currentCell);
+
+        int[][] path = new int[stck.size()][2];
+        for (int i=0; i<path.length; i++)
+        {
+            Point loc = (Point) stck.pop();
+            path[i][0] = (int) loc.getY();
+            path[i][1] = (int) loc.getX();
+        }
+
+        return path;
+    }
+
+    private static Cell find_path_bestFS(String[] sMaze)
+    {
         boolean[][] checkedCells = new boolean[sMaze.length][sMaze[0].length()];
-        Cell start, end;
-        start = end = new Cell(null, new Point(-1, -1));
+        Point start, end;
+        start = end = new Point(-1, -1);
         for (int i=0; i<sMaze.length; i++)
         {
             for (int j=0; j<sMaze[0].length(); j++)
@@ -89,17 +120,75 @@ public class MazeSolver //implememts IMazeSolver
                 checkedCells[i][j] = false;
                 switch (sMaze[i].charAt(j))
                 {
-                    case "S":
-
-                    case "E":
-
+                    case 'S':
+                    start = new Point(j, i);
+                    case 'E':
+                    end = new Point(j, i);
                 }
             }
         }
+        PQueue<Cell> Q = new PQueue<Cell>();
+        boolean endFound = false;
+        Cell currentCell = new Cell(null, start);
 
+        endFound = check_adjacents(currentCell, sMaze, checkedCells, end, Q);
 
+        while(!endFound && !Q.isEmpty())
+        {
+            currentCell = Q.remove_min();
+            endFound = check_adjacents(currentCell, sMaze, checkedCells, end, Q);
+        }
 
-        return new int[1][1];
+        Cell End = new Cell(currentCell, end);
+        return End;
+    }
+
+    private static boolean check_adjacents(Cell currentCell, String[] sMaze, boolean[][] checkedCells, Point end, PQueue<Cell> Q)
+    {
+        boolean endFound = false;
+        Direction checkingDirection = Direction.left;
+        Point nextLoc = new Point((int) currentCell.getX()-1, (int) currentCell.getY());
+        for (int i=0; i<4; i++)
+        {
+            if ((int) nextLoc.getX() >= 0 && (int) nextLoc.getX() < sMaze[0].length() && (int) nextLoc.getY() >= 0 && (int) nextLoc.getY() < sMaze.length)
+            {
+                switch(sMaze[(int) nextLoc.getY()].charAt((int) nextLoc.getX()))
+                {
+                    case '#':
+                    break;
+                    case 'E':
+                    endFound = true;
+                    return endFound;
+                    default:
+                    if (checkedCells[(int)nextLoc.getY()][(int)nextLoc.getX()]) break;
+                    Cell newCell = new Cell(currentCell, end);
+                    checkedCells[(int)nextLoc.getY()][(int)nextLoc.getX()] = true;
+                    /*
+                    The heuristic function calculates the minimum possible number of moves it would
+                    take to move from the current cell to the end of the maze. That is the score of that cell.
+                    The cells with the lowest scores are the most promising and so are checked first in the priority queue. 
+                    */
+                    int cellScore = (int) (Math.abs(currentCell.getX() - end.getX()) + Math.abs(currentCell.getY() - end.getY())); 
+                    Q.insert(newCell, cellScore);
+                }
+            }
+            
+            switch(checkingDirection)
+            {
+                case left:
+                checkingDirection = Direction.right;
+                nextLoc.setLocation(currentCell.getX()+1, currentCell.getY());
+                break;
+                case right:
+                checkingDirection = Direction.up;
+                nextLoc.setLocation(currentCell.getX(), currentCell.getY()-1);
+                break;
+                default://up case.
+                checkingDirection = Direction.down;
+                nextLoc.setLocation(currentCell.getX(), currentCell.getY()+1);
+            }
+        }
+        return endFound;
     }
 }
 
